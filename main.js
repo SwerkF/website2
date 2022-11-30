c422556eb4562e5fd29a9b16745dc7c9548da763
Oliwer Skweres <swerk.pro@gmail.com>
2022-11-30 17:06:00 +0100

    <Main>Update for E4

--- main.js
+++ main.js
@@ -20,3 +20,32 @@ var typed = new Typed('#title', {
 
 document.addEventListener("DOMContentLoaded", () => {
 });
+
+var checkScrollSpeed = (function(settings){
+  settings = settings || {};
+
+  var lastPos, newPos, timer, delta, 
+      delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
+
+  function clear() {
+    lastPos = null;
+    delta = 0;
+  }
+
+  clear();
+  
+  return function(){
+    newPos = window.scrollY;
+    if ( lastPos != null ){ // && newPos < maxScroll 
+      delta = newPos -  lastPos;
+    }
+    lastPos = newPos;
+    clearTimeout(timer);
+    timer = setTimeout(clear, delay);
+    return delta;
+  };
+})();
+
+// listen to "scroll" event
+window.onscroll = function(){
+};

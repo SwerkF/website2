235b00dde38f73b2c50e1f226ce63c0f8c2cfaba
Oliwer Skweres <swerk.pro@gmail.com>
2023-03-17 10:25:04 +0100

    <Fix>

--- main.js
+++ main.js
@@ -46,6 +46,18 @@ var checkScrollSpeed = (function(settings){
   };
 })();
 
-// listen to "scroll" event
-window.onscroll = function(){
-};
+
+var name = ["O", "l", "i", "w", "e", "r", "-", "S", "k", "w", "e", "r", "e", "s"];
+let idx = 0;
+let title;
+document.title = "";
+setInterval(() => {
+  if(idx > 13) {
+    idx = 1;
+    document.title = "O";
+  } else {
+    title = document.title + name[idx];
+    document.title = title;
+    idx++;
+  }
+}, 500)
